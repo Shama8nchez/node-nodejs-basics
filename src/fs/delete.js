e05fd453da33ca4046e3f63f59519a6efffc6412
Shama8nchez <shamanouski@gmail.com>
2023-05-09 20:40:21 +0300

    feat: add fs solutions

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { access, constants, unlink } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    // Write your code here
+    access(join(__dirname, 'files', 'fileToRemove.txt'), constants.F_OK, (err) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        } else {
+            unlink(join(__dirname, 'files', 'fileToRemove.txt'), (err) => {
+                if (err) {
+                    throw new Error('FS operation failed');
+                }
+            });
+        }
+    });
 };
 
 await remove();
