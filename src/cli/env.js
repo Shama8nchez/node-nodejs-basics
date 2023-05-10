5cc405a34f6e28adac625c6c93ee5927e2c449e7
Shama8nchez <shamanouski@gmail.com>
2023-05-10 14:36:53 +0300

    feat: add cli solutions

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
+import { env } from 'process';
+
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    console.log(Object.entries(env)
+        .filter(([key, value]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`)
+        .join('; '));
 };
 
 parseEnv();
