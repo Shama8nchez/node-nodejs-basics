9626cbe52fa54e23c4482d876a38b7ad5e6795ff
Shama8nchez <shamanouski@gmail.com>
2023-05-13 14:36:22 +0300

    feat: add streams solutions

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+const { stdin, stdout } = process;
+import { Transform } from 'stream';
+
+const myTransform = new Transform({
+    transform(chunk, encoding, callback) {
+        const result = chunk.toString().split('').reverse().join('');
+        callback(null, result + '\n');
+    }
+})
+
 const transform = async () => {
-    // Write your code here 
+    // Write your code here
+    stdout.write('Write something: ');
+    stdin.pipe(myTransform).pipe(stdout);
 };
 
 await transform();
