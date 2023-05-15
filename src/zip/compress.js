822cb3760083f9afaff37aa0132ef2900ff1afdd
Shama8nchez <shamanouski@gmail.com>
2023-05-15 14:33:17 +0300

    feat: add zip solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import zlib from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    const readStream = fs.createReadStream(join(__dirname, 'files', 'fileToCompress.txt'), 'utf-8');
+    const writeStream = fs.createWriteStream(join(__dirname, 'files', 'archive.gz'));
+    const gzip = zlib.createGzip();
+
+    readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
